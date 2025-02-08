b3b04a0afadae4834a5242b71346cfbe763a986b
FilipBabic <filip1986babic@gmail.com>
2025-02-08 11:36:27 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <div>
       <h1 className="h1-bold">Welcome to Next JS APP</h1>
       <h1 className="h1-bold font-space-grotesk">Welcome to Next JS APP</h1>
-      <p className="bg-primary-500 font-space-grotesk text-2xl">WELL DONE GIT SUCCESSFUll</p>
+      <p className="bg-primary-500 font-space-grotesk text-2xl">WELL DONE GIT</p>
     </div>
   );
 };
