18f17a250e5a97d7a8045c3fe9d89446c24ee2dc
FilipBabic <spotpackenter1@protonmail.com>
2025-02-08 10:47:36 +0100

    Fonts Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-black text-primary-500">Welcome to Next JS APP</h1>
-      <p>something 333</p>
+      <h1 className="h1-bold">Welcome to Next JS APP</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next JS APP</h1>
+      <p className="bg-primary-500 font-space-grotesk text-2xl">WELL DONE</p>
     </div>
   );
 };
