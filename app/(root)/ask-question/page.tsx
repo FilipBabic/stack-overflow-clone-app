2c4035060d821708a1f48ec5d13d2d888ab7ed9c
FilipBabic <filip1986babic@gmail.com>
2025-02-22 11:03:47 +0100

    Mobile Navigation and NavLinks

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return (
+    <div>
+      <p className="p-10">Ask s Question</p>
+    </div>
+  );
+};
+
+export default AskQuestion;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
@@ -10,15 +8,6 @@ const Home = async () => {
       <h1 className="h1-bold">Welcome to Next JS APP</h1>
       <h1 className="h1-bold font-space-grotesk">Welcome to Next JS APP</h1>
       <p className="bg-primary-500 font-space-grotesk text-2xl">WELL DONE GIT</p>
-      <form
-        className="px-10 pt-[200px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </div>
   );
 };
