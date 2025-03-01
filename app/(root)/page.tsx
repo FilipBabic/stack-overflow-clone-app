b838f65523f197f777e3734ed7fdced12f8b5987
FilipBabic <filip1986babic@gmail.com>
2025-03-01 11:17:35 +0100

    Implement Home filters

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -7,8 +8,8 @@ import ROUTES from "@/constants/routes";
 const questions = [
   {
     _id: "1",
-    title: "How to learn React",
-    description: "I want to learn React, can someone help me?",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
     tags: [
       { _id: "1", name: "React" },
       { _id: "2", name: "JavaScript" },
@@ -21,10 +22,10 @@ const questions = [
   },
   {
     _id: "2",
-    title: "How to learn JavaScript",
-    description: "I want to learn JavaScript, can someone help me?",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
     tags: [
-      { _id: "1", name: "React" },
+      { _id: "1", name: "JavaScript" },
       { _id: "2", name: "JavaScript" },
     ],
     author: { _id: "1", name: "John Doe" },
@@ -40,27 +41,34 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase())
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title.toLowerCase().includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
+  });
+
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark100_light900">Questions</h1>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
         <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900" asChild>
-          <Link href={ROUTES.ASK_QUESTION}>ASK A QUESTION</Link>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
       <section className="mt-11">
         <LocalSearch
           route="/"
           imgSrc="/icons/search.svg"
-          placeholder="Search questions"
+          placeholder="Search questions..."
           otherClasses="flex-1"
         />
       </section>
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
@@ -69,4 +77,5 @@ const Home = async ({ searchParams }: SearchParams) => {
     </>
   );
 };
+
 export default Home;
